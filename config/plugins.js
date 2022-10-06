798632218935ab9f7e43f581f2a067aa765e9d70
Alfred Lau <alfred@sulphur.com.sg>
2022-10-06 18:55:42 +0800

    :construction:

--- config/plugins.js
+++ config/plugins.js
@@ -23,4 +23,14 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  transformer: {
+    enabled: true,
+    config: {
+      prefix: "/api/",
+      responseTransforms: {
+        removeAttributesKey: true,
+        removeDataKey: true,
+      },
+    },
+  },
 });
