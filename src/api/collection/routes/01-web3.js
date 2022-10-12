4e06a5803364bbdd27d96275d610487893c29cae
Alfred Lau <alfred@sulphur.com.sg>
2022-10-12 20:12:03 +0800

    :wrench: added new routes for manually triggering metadata fetching + token fetching

--- src/api/collection/routes/01-web3.js
+++ src/api/collection/routes/01-web3.js
@@ -3,10 +3,17 @@ module.exports = {
     {
       method: "GET",
       path: "/collections/fetch/4c590453-68e8-49a5-b94b-3aababb9e5e2",
-      handler: "collection.fetchData",
+      handler: "collection.fetchAllData",
       config: {
         auth: false,
       },
+    },
+    {
+      method: "GET",
+      path: "GET",
+      path: "/collections/:id/fetch/4c590453-68e8-49a5-b94b-3aababb9e5e2",
+      handler: "collection.fetchData",
+      config: { auth: false },
     },
   ],
 };
