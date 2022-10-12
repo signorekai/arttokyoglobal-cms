1d9f185cbe4df6a42f168109e21f0aee9907507f
Alfred Lau <alfred@sulphur.com.sg>
2022-10-12 20:12:28 +0800

    :wrench: added controllers for routes

--- src/api/collection/controllers/collection.js
+++ src/api/collection/controllers/collection.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const Bottleneck = require("bottleneck");
+
 /**
  * collection controller
  */
@@ -9,7 +11,49 @@ const { createCoreController } = require("@strapi/strapi").factories;
 module.exports = createCoreController(
   "api::collection.collection",
   ({ strapi }) => ({
-    async fetchData(ctx, next) {
+    async fetchData(ctx) {
+      const metadataUpdates = {};
+      const result = await super.findOne(ctx);
+      const { data } = result;
+
+      const ABI = await strapi
+        .service("api::collection.web3")
+        .getABI(data.attributes.contractAddress);
+
+      if (ABI) {
+        const updates = await strapi
+          .service("api::collection.web3")
+          .readContract(data.attributes.contractAddress, ABI);
+
+        metadataUpdates.data = {
+          ABI,
+          ...updates,
+        };
+      }
+
+      const collectionUpdateRes = await strapi.entityService.update(
+        "api::collection.collection",
+        data.id,
+        metadataUpdates
+      );
+
+      console.log(`${data.attributes.contractAddress} - collection updated`);
+
+      if (collectionUpdateRes.CID) {
+        collectionUpdateRes.tokenUpdates = await strapi
+          .service("api::token.web3")
+          .fetchMetadataAndUpsert(
+            collectionUpdateRes.CID,
+            data.attributes.totalTokens,
+            { ...data.attributes, id: data.id }
+          );
+      }
+
+      ctx.type = "application/json";
+      ctx.body = JSON.stringify(collectionUpdateRes);
+      // await strapi.entityService.update()
+    },
+    async fetchAllData(ctx) {
       const collections = await strapi.entityService.findMany(
         "api::collection.collection",
         {
@@ -21,30 +65,47 @@ module.exports = createCoreController(
         }
       );
 
+      const limiter = new Bottleneck({
+        minTime: 333,
+        maxConcurrent: 1,
+      });
+
       const promises = collections.map((collection) => {
-        return new Promise((resolve, reject) => {
-          const { contractAddress, ABI } = collection;
-          console.log(`Fetching ${collection.contractAddress}`);
-          strapi
-            .service("api::collection.web3")
-            .readContract(contractAddress, ABI)
-            .then((updatedContract) =>
-              strapi.entityService.update(
+        return limiter.schedule(
+          () =>
+            new Promise(async (resolve, reject) => {
+              const { contractAddress, ABI } = collection;
+              console.log(`${collection.contractAddress} - fetching`);
+              const updatedContract = await strapi
+                .service("api::collection.web3")
+                .readContract(contractAddress, ABI);
+
+              const results = await strapi.entityService.update(
                 "api::collection.collection",
                 collection.id,
                 {
                   data: updatedContract,
                 }
-              )
-            )
-            .then((results) => {
+              );
+
+              console.log(`${contractAddress} - collection updated`);
+
+              if (results.CID) {
+                strapi
+                  .service("api::token.web3")
+                  .fetchMetadataAndUpsert(
+                    results.CID,
+                    results.totalTokens,
+                    results
+                  );
+              }
+
               console.log(
                 `Updated ${results.id} with ${results.contractAddress}`
               );
               resolve(results);
             })
-            .catch((err) => reject(err));
-        });
+        );
       });
 
       const updated = await Promise.all(promises);
@@ -55,7 +116,6 @@ module.exports = createCoreController(
       } catch (err) {
         ctx.body = err;
       }
-      next();
     },
   })
 );
