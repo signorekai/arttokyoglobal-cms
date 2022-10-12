325a5d5b2fe83816396df778f97c6a241826d6f3
Alfred Lau <alfred@sulphur.com.sg>
2022-10-13 01:29:04 +0800

    :sparkles: update and create hooks updated

--- src/api/collection/content-types/collection/lifecycles.js
+++ src/api/collection/content-types/collection/lifecycles.js
@@ -1,27 +1,82 @@
+const { v4: uuidv4 } = require("uuid");
+const slugify = require("slugify");
+
 module.exports = {
   async beforeUpdate(event) {
-    const { params } = event;
-    const id = params.where.id;
+    const { data, where } = event.params;
+    const { id } = where;
     const entry = await strapi.entityService.findOne(
       "api::collection.collection",
       id
     );
 
-    event.state.changedContractAddress =
-      params.data.contractAddress &&
-      entry.contractAddress !== params.data.contractAddress;
+    if (
+      data.contractAddress &&
+      entry.contractAddress !== data.contractAddress
+    ) {
+      const ABI = await strapi
+        .service("api::collection.web3")
+        .getABI(data.contractAddress);
+      if (ABI) {
+        const updates = await strapi
+          .service("api::collection.web3")
+          .readContract(data.contractAddress, ABI);
 
+        event.params.data = {
+          ...data,
+          ABI,
+          ...updates,
+        };
+      }
+
+      const tokens = await strapi
+        .service("api::token.web3")
+        .fetchMetadataAndUpsert(entry.CID, entry.totalTokens, entry);
+
+      const tokenIDs = tokens.map((token) => token.id);
+
+      data.tokens = tokenIDs;
+    }
+
+    return event;
+  },
+  async beforeCreate(event) {
+    const { data } = event.params;
+    const ABI = await strapi
+      .service("api::collection.web3")
+      .getABI(data.contractAddress);
+    if (ABI) {
+      const updates = await strapi
+        .service("api::collection.web3")
+        .readContract(data.contractAddress, ABI);
+
+      event.params.data = {
+        ...data,
+        ABI,
+        ...updates,
+      };
+    }
+
+    if (!data.slug) {
+      event.params.data.slug = `${slugify(event.params.data.title, {
+        lower: true,
+      })}-${uuidv4()}`;
+    }
+
+    return event;
+  },
+  async afterCreate(event) {
+    const { result: entry } = event;
+
+    const tokens = await strapi
+      .service("api::token.web3")
+      .fetchMetadataAndUpsert(entry.CID, entry.totalTokens, entry);
+
+    event.result.tokens = tokens;
     return event;
   },
   async afterUpdate(event) {
     const { state, result } = event;
-    if (state.changedContractAddress && result.contractAddress && result.CID) {
-      const tokens = await strapi
-        .service("api::token.web3")
-        .fetchMetadataAndUpsert(result.CID, result.totalTokens, result);
-
-      event.result.tokens = tokens;
-    }
     return event;
   },
 };
