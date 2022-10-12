053747bed02d28679ad09bf3ef75efca67df51bc
Alfred Lau <alfred@sulphur.com.sg>
2022-10-12 20:11:34 +0800

    :sparkles: added services for collection

--- src/api/collection/services/web3.js
+++ src/api/collection/services/web3.js
@@ -3,11 +3,8 @@
 const fetch = require("node-fetch");
 const ethers = require("ethers");
 const IPFSGatewayTools = require("@pinata/ipfs-gateway-tools/dist/node");
-const gatewayTools = new IPFSGatewayTools();
+const Bottleneck = require("bottleneck");
 
-/**
- * alchemy service
- */
 function tryParseJSONObject(jsonString) {
   try {
     var o = JSON.parse(jsonString);
@@ -58,6 +55,7 @@ module.exports = ({ strapi }) => ({
         // const re = new RegExp(
         //   /^ipfs:\/\/(Qm[1-9A-HJ-NP-Za-km-z]{44,}|b[A-Za-z2-7]{58,}|B[A-Z2-7]{58,}|z[1-9A-HJ-NP-Za-km-z]{48,}|F[0-9A-F]{50,})\/$/
         // );
+        const gatewayTools = new IPFSGatewayTools(); // better way of getting the CID
         const uriPrefix = await contract.uriPrefix();
         const results = gatewayTools.containsCID(uriPrefix);
         if (results.containsCid) {
@@ -67,5 +65,35 @@ module.exports = ({ strapi }) => ({
     }
 
     return updatedInfo;
+  },
+
+  async getAllTokenMetadata(CID, limit) {
+    console.log(`${CID} - getting all ${limit} JSONs`);
+
+    // follow rate limits of Pinata's Public Gateway
+    const limiter = new Bottleneck({
+      minTime: 100,
+      maxConcurrent: 4,
+    });
+
+    const files = [];
+    for (var x = 1; x <= limit; x++) {
+      files[x] = `${x}.json`;
+    }
+
+    try {
+      const tasks = files.map((file) =>
+        limiter.schedule(() =>
+          strapi.service("api::token.web3").fetchMetadata(CID, file)
+        )
+      );
+
+      const results = await Promise.all(tasks);
+      return results;
+    } catch (err) {
+      return {};
+      limiter.stop();
+    }
+    // console.log(CID, results.length);
   },
 });
