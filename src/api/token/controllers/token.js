d08e6996bc2c7ed58136fc1dc671ea553e2233d0
Alfred Lau <alfred@sulphur.com.sg>
2022-10-13 01:29:33 +0800

    :sparkles: added `fetchData` controller for token API route

--- src/api/token/controllers/token.js
+++ src/api/token/controllers/token.js
@@ -1,9 +1,23 @@
-'use strict';
+"use strict";
 
 /**
  * token controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::token.token');
+module.exports = createCoreController("api::token.token", ({ strapi }) => ({
+  async fetchData(ctx) {
+    const entry = await super.findOne(ctx);
+    const token = { id: entry.data.id, ...entry.data.attributes };
+
+    const metadata = await strapi
+      .service("api::token.web3")
+      .fetchMetadata(...token.ipfsUri.split("/"));
+
+    await strapi.service("api::token.web3").upsertMetadata(metadata);
+
+    ctx.type = "application/json";
+    ctx.body = JSON.stringify(metadata);
+  },
+}));
