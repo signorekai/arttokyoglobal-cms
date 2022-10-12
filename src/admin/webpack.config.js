24c9f65b07271a20c382a9dd3532b6b015f03862
Alfred Lau <alfred@sulphur.com.sg>
2022-10-13 01:27:28 +0800

    :wrench: added process.env.CLIENT_URL to pass config to admin panel

--- src/admin/webpack.config.js
+++ src/admin/webpack.config.js
@@ -1,9 +1,15 @@
-'use strict';
+"use strict";
 
 /* eslint-disable no-unused-vars */
 module.exports = (config, webpack) => {
   // Note: we provide webpack above so you should not `require` it
   // Perform customizations to webpack config
   // Important: return the modified config
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      "process.env.CLIENT_URL": JSON.stringify(process.env.CLIENT_URL),
+    })
+  );
+
   return config;
 };
