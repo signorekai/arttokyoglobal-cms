bfb8c20dec6b295840865f52342440baf7e3bd4c
Alfred Lau <alfred@sulphur.com.sg>
2022-11-22 16:16:16 +0800

    :hammer: remove whoami

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,7 +19,6 @@ module.exports = {
       ref: "origin/main",
       repo: "git@gitlab.com:alfredlau/art-tokyo-global-cms.git",
       path: "/var/www/cms",
-      "pre-deploy": "whoami",
       "post-setup": "npm install && npm run build:prod",
       "post-deploy":
         "npm install && npm run build:prod && pm2 startOrRestart ecosystem.config.js --env production",
